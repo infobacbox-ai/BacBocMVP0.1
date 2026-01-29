53bc204c19b675a6558e906460ea4cc2051da4d3
pierredelattreprof-ship-it <pierre.delattre.prof@gmail.com>
2026-01-29 19:14:33 +0100

    Revert "Remove remaining unused ts-expect-error directives in StatsTileChart"
    This reverts commit 28052e441d764ab2081e3bea6e8e7fe15c05fbe3.

--- apps/web/modules/saas/start/components/StatsTileChart.tsx
+++ apps/web/modules/saas/start/components/StatsTileChart.tsx
@@ -57,11 +57,13 @@ export function StatsTileChart({
 					axisLine={false}
 					tickMargin={8}
 				/>
+				{/* @ts-expect-error - Recharts components not compatible with React 19 types */}
 				<ChartTooltip
 					content={
 						<ChartTooltipContent formatter={tooltipFormatter} />
 					}
 				/>
+				{/* @ts-expect-error - Recharts components not compatible with React 19 types */}
 				<Area
 					dataKey={dataKey}
 					type="natural"
